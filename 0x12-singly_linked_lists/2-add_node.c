a94ceb551b3022fed08c1c2583634e66ac372221
ONOH SAMUEL <samuelonoh12345@gmail.com>
2022-10-21 00:54:09 +0100

    ok

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,53 +1,29 @@
 #include "lists.h"
-
 /**
- * _strlen - returns length of string
- * @s: string
- * Return: length of string
- */
-
-int _strlen(const char *s)
-{
-	int len;
-
-	len = 0;
-	while (s[len] != '\0')
-	{
-		len++;
-	}
-	return (len);
-}
-
-/**
- * add_node - adds a new node at the beginning of  list_t list
- * @head: head of linked list
- * @str: string to be used as data for node
- * Return: number of elements
+ * add_node - adds a new node at the beginning
+ * of a list_t list.
+ * @head: head of the linked list.
+ * @str: string to store in the list.
+ * Return: address of the head.
  */
 
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new;
-	char *string;
-	int length;
+    list_t *new;
+    size_t nchar;
 
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-		return (NULL);
-	if (str == NULL)
-	{
-		string = NULL;
-		length = 0;
-	}
-	else
-	{
-		string = strdup(str);
-		length = _strlen(str);
-	}
-	new->len = length;
-	new->str = string;
-	new->next = *head;
-	*head = new;
+    new = malloc(sizeof(list_t));
+    if (new == NULL)
+        return (NULL);
 
-	return (*head);
+    new->str = strdup(str);
+
+    for (nchar = 0; str[nchar]; nchar++)
+        ;
+
+    new->len = nchar;
+    new->next = *head;
+    *head = new;
+
+    return (*head);
 }
