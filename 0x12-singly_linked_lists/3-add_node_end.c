b62fccb57a2e86fb055a2b8ca8d3584dcb676f6f
ONOH SAMUEL <samuelonoh12345@gmail.com>
2022-10-21 00:53:23 +0100

    ok

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,66 +1,41 @@
 #include "lists.h"
 
 /**
- * _strlen - returns length of string
- * @s: string
- * Return: length of string
- */
-
-int _strlen(const char *s)
-{
-	int len;
-
-	len = 0;
-	while (s[len] != '\0')
-	{
-		len++;
-	}
-	return (len);
-}
-
-/**
- * add_node_end - adds a new node at the end of  list_t list
- * @head: head of linked list
- * @str: string to be used as data for node
- * Return: number of elements
+ * add_node_end - adds a new node at the end
+ * of a list_t list.
+ * @head: head of the linked list.
+ * @str: string to store in the list.
+ * Return: address of the head.
  */
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new;
-	char *string;
-	list_t *temp;
+    list_t *new, *temp;
+    size_t nchar;
 
-	temp = *head;
-	new = malloc(sizeof(list_t));
-	if (new == NULL)
-		return (NULL);
+    new = malloc(sizeof(list_t));
+    if (new == NULL)
+        return (NULL);
 
-	if (str == NULL)
-		return (NULL);
+    new->str = strdup(str);
 
-	string = strdup(str);
-	if (string == NULL)
-	{
-		free(new);
-		return (NULL);
-	}
+    for (nchar = 0; str[nchar]; nchar++)
+        ;
 
-	new->len = _strlen(str);
-	new->str = string;
-	new->next = NULL;
+    new->len = nchar;
+    new->next = NULL;
+    temp = *head;
 
-	if (*head == NULL)
-	{
-		*head = new;
-		return (new);
-	}
+    if (temp == NULL)
+    {
+        *head = new;
+    }
+    else
+    {
+        while (temp->next != NULL)
+            temp = temp->next;
+        temp->next = new;
+    }
 
-	while (temp->next != NULL)
-	{
-		temp = temp->next;
-	}
-	temp->next = new;
-
-	return (new);
+    return (*head);
 }
