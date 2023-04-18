3186e8cef181e1c5c2b9f325def5d6e6690dab5b
ONOH SAMUEL <samuelonoh12345@gmail.com>
2023-04-18 05:04:49 +0100

    task done

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,10 +12,10 @@
 int linear_search(int *array, size_t size, int value)
 {
 	int i;
-
+// return
 	if (array == NULL)
 		return (-1);
-
+// loop
 	for (i = 0; i < (int)size; i++)
 	{
 		printf("Value checked array[%u] = [%d]\n", i, array[i]);
